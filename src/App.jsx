5f738d6041a4a16a20bc49640640f398cce403f0
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-08 13:25:12 +0530

    weather-ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Weather from "./components/weather";
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+      <Weather />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/weather.jsx
@@ -0,0 +1,34 @@
+import { IoSearch } from "react-icons/io5";
+
+const Weather = () => {
+  return (
+    <main className="h-screen w-screen flex items-start justify-center">
+      <div className="flex items-center justify-center gap-8 m-12 text-xl">
+        <select
+          name="cities"
+          id="cities"
+          className="h-10 w-56 rounded-md outline-none"
+        >
+          <option value="">Cities</option>
+          <option>City</option>
+          <option>City</option>
+          <option>City</option>
+          <option>City</option>
+        </select>
+        <div className="flex items-center justify-center gap-">
+          <input
+            type="text"
+            placeholder="Search"
+            className="h-10 w-56 rounded-l-lg outline-none p-2 capitalize"
+          />
+          <button className="flex items-center justify-center bg-black text-white h-10 w-10 rounded-r-lg">
+            <IoSearch />
+          </button>
+        </div>
+      </div>
+      <div></div>
+    </main>
+  );
+};
+
+export default Weather;
